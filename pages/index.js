e0b0bc83f8745f52a942d7c11efb747c1f4bd744
Tahir Yasin <tahiryasin123@gmail.com>
2023-12-03 18:23:12 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Pakistantrailsadventure.com | Top Travel & Tours website from Pakistan.</title>
+        <title>PakistanTrailsAdventure.com | Top Travel & Tours website from Pakistan.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
